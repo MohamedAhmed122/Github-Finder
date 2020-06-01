94896ab0b12c599fe673a1672ec594373a21fe0b
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-06-01 14:50:26 +0700

    build search form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import React from "react";
 import "./App.css";
-import User from "./Component/Users/Users.component";
 import axios from "axios";
+import User from "./Component/Users/Users.component";
+import Search from './Component/SearhForm/SearchForm'
 import Navbar from "./Component/NavBar/NavBar.component";
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
+    showCase:true
   };
+
   async componentDidMount() {
-    this.setState({ loading: true });
+    this.setState({
+      loading: true
+    });
     const response = await axios(`https://api.github.com/users?client_id=${process.env.CLIENT_ID }&client_secret=${process.env.CLIENT_SECRET}`);
     console.log(response.data);
-    this.setState({ users: response.data, loading: false });
+    this.setState({
+      users: response.data,
+      loading: false,
+    });
+  }
+  searchUser = async query => {
+    console.log(query);
+    this.setState({
+      loading: true
+    });
+    const response = await axios(`https://api.github.com/search/users?q=${query}&client_id=${process.env.CLIENT_ID }&client_secret=${process.env.CLIENT_SECRET}`);
+    console.log(response.data);
+    this.setState({
+      users: response.data.items,
+      loading: false,
+      showCase: false
+    });
+  }
+  clearUsers = async() =>{
+    const response = await axios(`https://api.github.com/users?client_id=${process.env.CLIENT_ID }&client_secret=${process.env.CLIENT_SECRET}`);
+    this.setState({
+      users: response.data,
+      loading: false,
+      showCase:true
+    });
   }
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Search clearUsers={this.clearUsers} searchUser={this.searchUser} showCase={this.state.showCase}/>
         <User users={this.state.users} loading={this.state.loading} />
-        <h2>Hello from react</h2>
-      </div> 
+    </div> 
     );
   }
 }
---
+++ src/Component/SearhForm/SearchForm.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class SearchForm extends Component {
+  state = {
+    search: "",
+  };
+  handleSumbit = (event) => {
+    event.preventDefault();
+    // how to pass props to the the Appjs
+    this.props.searchUser(this.state.search);
+    this.setState({});
+  };
+  handleChange = (event) => {
+    this.setState({ search: event.target.value });
+  };
+  render() {
+    const { showCase, clearUsers } = this.props;
+    return (
+      <div className="container">
+        <form className="form" onSubmit={this.handleSumbit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+        {!showCase ? (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        ) : null}
+        {/* another way to send the props to other component */}
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
