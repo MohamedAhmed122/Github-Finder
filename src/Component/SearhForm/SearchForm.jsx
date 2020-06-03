c61ae164c7d53cc03af9ebb6ee36522557c07a7f
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-06-03 18:33:21 +0700

    Convert the SearchForm and the User component to functional component useing hooks

--- src/Component/SearhForm/SearchForm.jsx
+++ src/Component/SearhForm/SearchForm.jsx
@@ -1,49 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class SearchForm extends Component {
-  state = {
-    search: "",
-  };
-  handleSumbit = (event) => {
+const SearchForm = ({ showCase, clearUsers, setAlert, searchUser }) => {
+  const [ search, setSearch]  = useState("");
+
+  const handleSumbit = (event) => {
     event.preventDefault();
-    if(this.state.search === ''){
-        this.props.setAlert('Please, enter text in the search bar', 'light');
+    if (search === "") {
+      setAlert("Please, enter text in the search bar", "light");
+    } else {
+      // how to pass props to the the Appjs
+      searchUser(search);
+      setSearch('');
     }
-    // how to pass props to the the Appjs
-    this.props.searchUser(this.state.search);
-    this.setState({search: ''});
   };
-  handleChange = (event) => {
-    this.setState({ search: event.target.value });
-    
+  const handleChange = (event) => {
+    setSearch(event.target.value);
   };
-  render() {
-    const { showCase, clearUsers } = this.props;
-    return (
-      <div className="container">
-        <form className="form" onSubmit={this.handleSumbit}>
-          <input
-            type="text"
-            name="text"
-            placeholder="Search Users..."
-            value={this.state.search}
-            onChange={this.handleChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {!showCase ? (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        ) : null}
-        {/* another way to send the props to other component */}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSumbit}>
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={search}
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {!showCase ? (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      ) : null}
+      {/* another way to send the props to other component */}
+    </div>
+  );
+};
 
 export default SearchForm;
--- src/Component/User/User.jsx
+++ src/Component/User/User.jsx
@@ -1,107 +1,113 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Spinner from "../Spinner/Spinner.compent";
 
-import Repo from '../Repos/Repo'
+import Repo from "../Repos/Repo";
 
 import { Link } from "react-router-dom";
 
-class User extends React.Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getRepos(this.props.match.params.login);
-  }
-  render() {
-    const {
-      login,
-      avatar_url,
-      html_url,
-      name,
-      blog,
-      location,
-      hireable,
-      bio,
-      public_repos,
-      followers,
-      following,
-      company,
-      public_gists
-    } = this.props.user;
-    if (this.props.loading) return <Spinner />;
-    return (
-      <div className="container" style={{ marginTop: "50px" }}>
-        <div className="my-2">
-          <Link to="/" className="btn btn-dos">
-            Back to Search
-          </Link>
-          Hireable:
-          {hireable ? (
-            <i className="fa fa-check text-success" />
-          ) : (
-            <i className="fa fa-times-circle text-danger" />
-          )}
-        </div>
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              alt="avatar"
-              className="round-img"
-              style={{ width: "200px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location : {location} </p>
-          </div>
-          <div>
-            {bio ? (
-              <div>
-                <h3>Bio</h3>
-                <p>{bio}</p>{" "}
-              </div>
-            ) : null}
-            <a href={html_url} className="btn btn-dark my-2">
-              Visit my Github Profile
-            </a>
-            <ul>
-              <li>
-                {login ? (
-                  <div>
-                    {" "}
-                    <strong>username:  </strong>
-                    {login}{" "}
-                  </div>
-                ) : null}
-              </li>
-              <li>
-                {blog ? (
-                  <div>
-                    {" "}
-                    <strong>Blog:  </strong>
-                    {blog}{" "}
-                  </div>
-                ) : null}
-              </li>
-              <li>
-                {company ? (
-                  <div>
-                    {" "}
-                    <strong>company:  </strong>
-                    {company}{" "}
-                  </div>
-                ) : null}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className='card text-center'>
-            <div className='badge badge-primary' >Followers:  {followers}</div>
-            <div className='badge badge-success' >Following:  {following}</div>
-            <div className='badge badge-primary2' >Public Repo:  {public_repos}</div>
-            <div className='badge badge-dark' >Public  Gists:  {public_gists}</div>
-        </div>
-        <Repo repos={this.props.repos} />
+
+const User = ({ getUser, getRepos, loading, repos, user, match }) => {
+
+  useEffect(()=>{
+    getUser(match.params.login);
+    getRepos(match.params.login);
+  },[])
+
+  // componentDidMount() {
+  //   getUser(match.params.login);
+  //   getRepos(match.params.login);
+  // }
+
+  const {
+    login,
+    avatar_url,
+    html_url,
+    name,
+    blog,
+    location,
+    hireable,
+    bio,
+    public_repos,
+    followers,
+    following,
+    company,
+    public_gists} = user;
+
+  if (loading) return <Spinner />;
+  return (
+    <div className="container" style={{ marginTop: "50px" }}>
+      <div className="my-2">
+        <Link to="/" className="btn btn-dos">
+          Back to Search
+        </Link>
+        Hireable:
+        {hireable ? (
+          <i className="fa fa-check text-success" />
+        ) : (
+          <i className="fa fa-times-circle text-danger" />
+        )}
       </div>
-    );
-  }
-}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt="avatar"
+            className="round-img"
+            style={{ width: "200px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location : {location} </p>
+        </div>
+        <div>
+          {bio ? (
+            <div>
+              <h3>Bio</h3>
+              <p>{bio}</p>{" "}
+            </div>
+          ) : null}
+          <a href={html_url} className="btn btn-dark my-2">
+            Visit my Github Profile
+          </a>
+          <ul>
+            <li>
+              {login ? (
+                <div>
+                  {" "}
+                  <strong>username: </strong>
+                  {login}{" "}
+                </div>
+              ) : null}
+            </li>
+            <li>
+              {blog ? (
+                <div>
+                  {" "}
+                  <strong>Blog: </strong>
+                  {blog}{" "}
+                </div>
+              ) : null}
+            </li>
+            <li>
+              {company ? (
+                <div>
+                  {" "}
+                  <strong>company: </strong>
+                  {company}{" "}
+                </div>
+              ) : null}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-primary2">Public Repo: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repo repos={repos} />
+    </div>
+  );
+};
 export default User;
